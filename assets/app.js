ab018074a14490f89569f89d7201af18cebb52f4
jonnhyOrtega <jonnhyortega@gmail.com>
2024-03-09 22:27:59 -0300

    agrega carro de compras y funcionalidades

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,4 @@
+// DESPLEGAR MENU
 const nav = document.getElementById("navegador-mobile");
 
 const desplegarMenu = () => {
@@ -7,8 +8,100 @@ const desplegarMenu = () => {
     nav.classList.add("desplegado");
   }
 };
+// DESPLEGAR MENU
 
 
+// PRODUCTOS
+
+const productos = [
+  {
+    id: 1,
+    nombre: "Botiquin Marron",
+    precio: 1000,
+    imagen: "/assets/imgs/productos/botiquin-marron.jpeg",
+  },
+  {
+    id: 2,
+    nombre: "Botiquin Blanco",
+    precio: 1000,
+    imagen: "/assets/imgs/productos/botiquin-blanco.jpeg",
+  },
+  {
+    id: 3,
+    nombre: "Divisor Blanco",
+    precio: 1000,
+    imagen: "/assets/imgs/productos/divisor-blanco.jpeg",
+  },
+  {
+    id: 4,
+    nombre: "Divisor Opaco",
+    precio: 1000,
+    imagen: "/assets/imgs/productos/divisor-opaco.jpeg",
+  },
+  {
+    id: 5,
+    nombre: "Divisor Opaco plastico",
+    precio: 1000,
+    imagen: "/assets/imgs/productos/divisor-opaco-plastico.jpeg",
+  },
+];
+
+const productosContainer = document.querySelector("#productos-container");
+
+function cargarProductos() {
+  productos.forEach((producto) => {
+    const div = document.createElement("div");
+    div.classList.add("card");
+    div.innerHTML = `
+    <img
+    id="producto-img"
+    src="${producto.imagen}"
+    alt="${producto.nombre}"
+  />
+  
+  <div class="box-card">
+    <span id="price">$${producto.precio}</span>
+    <span id="descuento">-15%</span>
+  </div>
+  
+  <span id="description">${producto.nombre}</span>
+  
+  <div class="box-card-button">
+    <button onclick="agregarProducto();" id="${producto.id}" class="agregar">Agregar</button>
+    <i class="fa-solid fa-cart-shopping"></i>
+  </div>
+    `;
+    productosContainer.append(div);
+  });
+}
+
+cargarProductos();
+// PRODUCTOS
+
+// Funciones CARRITO de compras
+
+// DESPLEGAR
+const carritoDeCompras = document.getElementById("productos-carrito");
+
+const abrirCarrito = () => {
+  if (carritoDeCompras.classList.contains("carrito-desplegado")) {
+    carritoDeCompras.classList.remove("carrito-desplegado");
+  } else {
+    carritoDeCompras.classList.add("carrito-desplegado");
+  }
+};
+// DESPLEGAR
+
+// Agregar productos
+const carroDeCompras = document.getElementById("productos-agregados");
+const arrayDeProductos = [];
+const agregarProducto = (producto) => {
+  arrayDeProductos.push(productos[producto]);
+};
+
+// Funciones CARRITO de compras
+
+// MOSTRAR PASSWORD DE LOGIN
 function mostrarPassword() {
   var passwordInput = document.getElementById("password-login");
   var icon = document.getElementById("button-show-password");
@@ -24,44 +117,3 @@ function mostrarPassword() {
   }
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-  var cardsContainer = document.querySelector(".cards-container");
-  var verMasButton = document.getElementById("verMas");
-  var cards = document.querySelectorAll(".card");
-  var mostrarPorClick = 4; // Cantidad de tarjetas a mostrar por clic
-  var indiceInicio = 0;
-
-  // Ocultar todas las tarjetas al cargar la página
-  ocultarTarjetas();
-
-  // Función para ocultar todas las tarjetas
-  function ocultarTarjetas() {
-    cards.forEach(function (card) {
-      card.style.display = "none";
-    });
-  }
-
-  // Función para mostrar las siguientes tarjetas
-  function mostrarSiguientesTarjetas() {
-    var i;
-    for (i = indiceInicio; i < indiceInicio + mostrarPorClick; i++) {
-      if (cards[i]) {
-        cards[i].style.display = "block";
-      }
-    }
-    indiceInicio += mostrarPorClick;
-
-    // Ocultar botón si no hay más tarjetas por mostrar
-    if (indiceInicio >= cards.length) {
-      verMasButton.style.display = "none";
-    }
-  }
-
-  // Mostrar las primeras tarjetas al cargar la página
-  mostrarSiguientesTarjetas();
-
-  // Evento click del botón "Ver más"
-  verMasButton.addEventListener("click", function () {
-    mostrarSiguientesTarjetas();
-  });
-});
